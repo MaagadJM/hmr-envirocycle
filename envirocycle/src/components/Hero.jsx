e8f4153d2a298edac24618dcb3c96c881f4d3c24
Jude Michael Maagad <judemichael.maagad@hmr.ph>
2026-08-14 13:09:55 +0800

    hotfix: style - main section title

--- envirocycle/src/components/Hero.jsx
+++ envirocycle/src/components/Hero.jsx
@@ -14,7 +14,7 @@ export default function Hero() {
       <div className="absolute inset-0 flex items-center px-12 md:px-20 lg:px-28">
         <div className="w-full text-center grid grid-cols-2">
           {/* Left column — text */}
-          <div className="text-white font-extralight text-4xl md:text-7xl leading-none drop-shadow-lg tracking-tighter">
+          <div className="text-white font-extralight text-4xl md:text-6xl leading-none drop-shadow-lg tracking-tighter">
             <div className="absolute left-54 top-1/2 transform -translate-y-full">
               Building a Circular
               {/* <h1>
